6696ae890c9b691bf478ed38cfa2643d6185424b
sky7777 <mbawany@tepper.cmu.edu>
2023-06-15 19:02:20 +0100

    gg

--- index.ts
+++ index.ts
@@ -15,7 +15,9 @@
 import express, {Express, Request, Response} from 'express';
 import 'dotenv/config'
 import LemmyBot , { Vote }  from 'lemmy-bot';
-
+import { Login, LemmyHttp , GetCommunity, EditCommunity} from 'lemmy-js-client';
+import { describe } from 'node:test';
+//import createTable from  'json-to-markdown-table'
 
 const PORT = process.env.PORT || 8021;
 const app: Express = express();
@@ -94,10 +96,92 @@ const lemmywinx = new LemmyBot({
 });
 lemmywinx.start();
 
+
+
+
+
+
 app.get('/', (req: Request, res: Response) => {
+  //res.send('🎉 Hello TypeScript! 🎉');
+
+  res.send('the user name of the bot is:- '+process.env.USERNAME);
+});
+
+let form: Login = {
+  username_or_email: process.env.USERNAME || "",
+  password: process.env.PASSWORD || ""
+}
+let baseURL = 'Https://lemmy.world';
+let client : LemmyHttp = new LemmyHttp(baseURL);
+const login_response = new Promise((resolve, reject) => {
+  resolve(client.login(form));
+});
+let community_id_form: GetCommunity ={name: 'sky_7_bot_testing' }
+/*const community_id_response = new Promise((resolve, reject) =>
+{
+  resolve(client.getCommunity(community_id_form))
+}
+) 
+community_id_response.then(value => {console.log(value)})
+/* Id was 9451 of the tet site 
+*/
+
+function reform_fixtures(): {Home: string; Away: string; score: string; date: string }[] {
+  return [{Home: "Manchester United",
+          Away:  "Spurs",
+          score: "3-1",
+          date: "2022-02-01"
+        },
+        {Home: "Manchester United",
+          Away:  "Chelsea",
+          score: "5-1",
+          date: "2023-02-01"
+        },
+      ];
+}
+
+function turn_into_markdown_string(something:any): string {
+  const markdown_thing = require('json-to-markdown-table')
+  const columns = ['Home', 'Away','Score','Date']
+  return markdown_thing(
+    [{Home: "Manchester United",
+          Away:  "Spurs",
+          score: "3-1",
+          date: "2022-02-01"
+        },
+        {Home: "Manchester United",
+          Away:  "Chelsea",
+          score: "5-1",
+          date: "2023-02-01"
+        },
+      ], columns
+    )
+}
+
+login_response.then( (value:any) => {
+  let jwt = value.jwt;
+  console.log("the token is : "+jwt);
+  let new_md_output = turn_into_markdown_string(reform_fixtures())
+  console.log(new_md_output)
+  let edit_community_form: EditCommunity = {
+    auth: jwt,
+    community_id : 9451,
+     description : new_md_output
+  }
+  const editcommunity = new Promise((resolve, reject) => {
+    resolve(client.editCommunity(edit_community_form));
+  });
+}).then( (value:any) =>{
+  console.log("success")
+}
+);
+
+
+app.get('/granger', (req: Request, res: Response) => {
   //res.send('🎉 Hello TypeScript! 🎉');
   res.send('the user name of the bot is:- '+process.env.USERNAME);
 });
+
 
 const server = app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
