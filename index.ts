307bd50d5826e5dbb0ddc89d7ef77290ccaa2935
sky7777 <mbawany@tepper.cmu.edu>
2023-06-16 20:55:17 +0100

    fixed all issues with sending fixtures. Testing

--- index.ts
+++ index.ts
@@ -12,11 +12,12 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 import 'dotenv/config'
-import LemmyBot , { Vote }  from 'lemmy-bot';
-import { Login, LemmyHttp , GetCommunity, EditCommunity} from 'lemmy-js-client';
+import LemmyBot, { Vote } from 'lemmy-bot';
+import { Login, LemmyHttp, GetCommunity, EditCommunity } from 'lemmy-js-client';
 import { describe } from 'node:test';
+import { get_list_of_fixtures } from './src/fixtures';
 //import createTable from  'json-to-markdown-table'
 
 const PORT = process.env.PORT || 8021;
@@ -29,9 +30,9 @@ const lemmywinx = new LemmyBot({
     password: process.env.PASSWORD || ""
   },
   connection: {
-    minutesUntilReprocess : 10
+    minutesUntilReprocess: 10
   },
-  federation:{
+  federation: {
     allowList: [
       {
         instance: 'lemmy.world',
@@ -40,146 +41,117 @@ const lemmywinx = new LemmyBot({
     ]
   },
   handlers: {
-    post: (res) =>{
+    post: (res) => {
       console.log(res.postView.post.name);
     },
-  /*  mention : ({
-      mentionView:
-        {
-            comment: {creator_id, id}
-          },
-          botActions: { voteComment }
-        }) => {
-        voteComment(Vote.Upvote);
-      }
-   */
-    mention : ({
-      mentionView:
-        {
-            comment: {creator_id, id:comment_id, content}
-            , post: {id}
-          },
-          botActions: {createComment, voteComment}
-
-        }) => {
-          createComment({
-            "content": "Hi there, Lemmywinx reporting for duty",
-            "postId": id,
-            "parentId" : comment_id
-          })
-        console.log(creator_id, id, content);
-      }
-   
-
-      //  mention : (res) => {
-
-      
-//    console.log(res.mentionView.comment.content)
-    
- // }
+    /*  mention : ({
+        mentionView:
+          {
+              comment: {creator_id, id}
+            },
+            botActions: { voteComment }
+          }) => {
+          voteComment(Vote.Upvote);
         }
+     */
+    mention: ({
+      mentionView:
+      {
+        comment: { creator_id, id: comment_id, content }
+        , post: { id }
+      },
+      botActions: { createComment, voteComment }
 
-      
-    
-      
-      
-
-    
-
-
-  
-     
+    }) => {
+      createComment({
+        "content": "Hi there, Lemmywinx reporting for duty",
+        "postId": id,
+        "parentId": comment_id
+      })
+      console.log(creator_id, id, content);
+    }
 
 
+    //  mention : (res) => {
 
+
+    //    console.log(res.mentionView.comment.content)
+
+    // }
+  }
 
 });
 lemmywinx.start();
 
 
+// Auto Create Posts
 
+
+//
 
 
 
 app.get('/', (req: Request, res: Response) => {
   //res.send('🎉 Hello TypeScript! 🎉');
 
-  res.send('the user name of the bot is:- '+process.env.USERNAME);
+  res.send('the user name of the bot is:- ' + process.env.USERNAME);
 });
 
-let form: Login = {
-  username_or_email: process.env.USERNAME || "",
-  password: process.env.PASSWORD || ""
-}
-let baseURL = 'Https://lemmy.world';
-let client : LemmyHttp = new LemmyHttp(baseURL);
-const login_response = new Promise((resolve, reject) => {
-  resolve(client.login(form));
-});
-let community_id_form: GetCommunity ={name: 'sky_7_bot_testing' }
-/*const community_id_response = new Promise((resolve, reject) =>
-{
-  resolve(client.getCommunity(community_id_form))
-}
-) 
-community_id_response.then(value => {console.log(value)})
-/* Id was 9451 of the tet site 
-*/
 
-function reform_fixtures(): {Home: string; Away: string; score: string; date: string }[] {
-  return [{Home: "Manchester United",
-          Away:  "Spurs",
-          score: "3-1",
-          date: "2022-02-01"
-        },
-        {Home: "Manchester United",
-          Away:  "Chelsea",
-          score: "5-1",
-          date: "2023-02-01"
-        },
-      ];
-}
 
-function turn_into_markdown_string(something:any): string {
-  const markdown_thing = require('json-to-markdown-table')
-  const columns = ['Home', 'Away','Score','Date']
-  return markdown_thing(
-    [{Home: "Manchester United",
-          Away:  "Spurs",
-          score: "3-1",
-          date: "2022-02-01"
-        },
-        {Home: "Manchester United",
-          Away:  "Chelsea",
-          score: "5-1",
-          date: "2023-02-01"
-        },
-      ], columns
-    )
-}
-
-login_response.then( (value:any) => {
-  let jwt = value.jwt;
-  console.log("the token is : "+jwt);
-  let new_md_output = turn_into_markdown_string(reform_fixtures())
-  console.log(new_md_output)
-  let edit_community_form: EditCommunity = {
-    auth: jwt,
-    community_id : 9451,
-     description : new_md_output
+ // ======================================
+  // Update Fixtures
+  // ======================================
+  
+  
+function update_fixture_table() {
+  let form: Login = {
+    username_or_email: process.env.USERNAME || "",
+    password: process.env.PASSWORD || ""
   }
-  const editcommunity = new Promise((resolve, reject) => {
-    resolve(client.editCommunity(edit_community_form));
+  let baseURL = 'Https://lemmy.world';
+  let client: LemmyHttp = new LemmyHttp(baseURL);
+  const login_response = new Promise((resolve, reject) => {
+    resolve(client.login(form));
   });
-}).then( (value:any) =>{
-  console.log("success")
-}
-);
+  let community_id_form: GetCommunity = { name: 'sky_7_bot_testing' }
+  /*const community_id_response = new Promise((resolve, reject) =>
+  {
+    resolve(client.getCommunity(community_id_form))
+  }
+  ) 
+  community_id_response.then(value => {console.log(value)})
+  /* Id was 9451 of the tet site 
+  */
+ login_response.then((value: any) => {
+    let jwt = value.jwt;
+    console.log("the token is : " + jwt);
+    let test_string = ''
+    const new_md_output = new Promise<string>((resolve, reject) => {
+      resolve(get_list_of_fixtures());
+    });
+    new_md_output.then(value => {
 
+      let edit_community_form: EditCommunity = {
+        auth: jwt,
+        community_id: 9451,
+        description: value
+      }
+      setTimeout(function () {
+        client.editCommunity(edit_community_form);
+        console.log(`Value of the table is\n+++++++++++++++\n${value}`);
+      }, 5000)
+    })
+  })
+}
+
+// ======================================
+// End Update Fixtures
+// ======================================
 
 app.get('/granger', (req: Request, res: Response) => {
   //res.send('🎉 Hello TypeScript! 🎉');
-  res.send('the user name of the bot is:- '+process.env.USERNAME);
+  res.send('the user name of the bot is:- ' + process.env.USERNAME);
 });
 
 
---
+++ src/test.ts
@@ -0,0 +1,24 @@
+
+import { resolve } from "dns/promises";
+import { setTimeout } from "timers";
+
+async function james() {
+    let xyz = await Promise.resolve(leeway)
+    console.log("james is about to return")
+    return xyz 
+
+}
+
+let kk_state = 'Pending'
+
+let leeway = new Promise<string>((resolve, reject) => {setTimeout(() => {
+    console.log('Promise Started');
+    kk_state = 'resolved';
+    resolve('This is done');
+    
+}, 5000);}).catch(err => console.log(err));
+    //.then(value => console.log(value))
+async function letter (){
+    return await james()
+}
+letter()
