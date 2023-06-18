dfa4b3f188529bee6c61a59662998c60450f96ed
sky7777 <mbawany@tepper.cmu.edu>
2023-06-18 04:46:07 +0100

    init_code_for_cyclic

---
+++ index.js
@@ -0,0 +1,228 @@
+"use strict";
+// Copyright 2018 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+require("dotenv/config");
+const lemmy_bot_1 = __importDefault(require("lemmy-bot"));
+const lemmy_js_client_1 = require("lemmy-js-client");
+const fixtures_1 = require("./src/fixtures");
+const { google } = require('googleapis');
+//import createTable from  'json-to-markdown-table'
+let jwt = '';
+const PORT = process.env.PORT || 8021;
+const app = (0, express_1.default)();
+const post_template = {
+    name: "🍻 The Lounge: Daily Discussion Thread",
+    community_id: 9451,
+    body: "This is a post from Lemmywinkx u/frostbot",
+    auth: ''
+};
+const baseURL = 'Https://lemmy.world';
+let client = new lemmy_js_client_1.LemmyHttp(baseURL);
+/**
+ * Import the GoogleAuth library, and create a new GoogleAuth client.
+ */
+const { GoogleAuth } = require('google-auth-library');
+/**
+ * Acquire a client, and make a request to an API that's enabled by default.
+ */
+async function access_sheets(
+// Full path to the sevice account credential
+keyFile = process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+    const auth = new GoogleAuth({
+        keyFile: keyFile,
+        scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+    });
+    const sheets = google.sheets({ version: 'v4', auth });
+    const res = await sheets.spreadsheets.values.get({
+        spreadsheetId: '1-JfjQB1m1EjsrbP3K4cW1r9rQT6d0Ztnvm5T1XRj_yk',
+        range: 'fixtures!A1:B1',
+    });
+    //console.log(res)
+    return await sheets;
+}
+/*
+function get_some_values_from_google_sheets (sheets){
+  console.log(`Start of get value function value is ${sheets}`)
+  sheets.then(value =>
+    {
+      console.log(`Start of get value function value is ${value}`)
+    new Promise ((resolve, reject) =>{
+      console.log(`Start of get value function value is ${value}`)
+      resolve(value.spreadsheets.values.get({
+        spreadsheetId: '1-JfjQB1m1EjsrbP3K4cW1r9rQT6d0Ztnvm5T1XRj_yk',
+        range: 'fixtures!A1:D1',
+      });)
+    }).then(value =>{
+      console.log(value)
+     })
+    
+  
+})
+
+}
+*/
+const lemmywinx = new lemmy_bot_1.default({
+    instance: 'lemmy.world',
+    credentials: {
+        username: process.env.USERNAME || "",
+        password: process.env.PASSWORD || ""
+    },
+    connection: {
+        minutesUntilReprocess: 10
+    },
+    federation: {
+        allowList: [
+            {
+                instance: 'lemmy.world',
+                communities: ['sky_7_bot_testing']
+            }
+        ]
+    },
+    handlers: {
+        post: (res) => {
+            console.log(res.postView.post.name);
+        },
+        /*  mention : ({
+            mentionView:
+              {
+                  comment: {creator_id, id}
+                },
+                botActions: { voteComment }
+              }) => {
+              voteComment(Vote.Upvote);
+            }
+         */
+        mention: ({ mentionView: { comment: { creator_id, id: comment_id, content }, post: { id } }, botActions: { createComment, voteComment, createPost } }) => {
+            createComment({
+                "content": "Hi there, Lemmywinx reporting for duty",
+                "postId": id,
+                "parentId": comment_id
+            });
+            //createPost(post_template)
+            console.log(creator_id, id, content);
+        }
+        //  mention : (res) => {
+        //    console.log(res.mentionView.comment.content)
+        // }
+    }
+});
+lemmywinx.start();
+//
+async function js_client_login(community_name) {
+    let form = {
+        username_or_email: process.env.USERNAME || "",
+        password: process.env.PASSWORD || ""
+    };
+    const login_response = new Promise((resolve, reject) => {
+        resolve(client.login(form));
+    });
+    let community_id_form = { name: community_name };
+    /*const community_id_response = new Promise((resolve, reject) =>
+    {
+      resolve(client.getCommunity(community_id_form))
+    }
+    )
+    community_id_response.then(value => {console.log(value)})
+    /* Id was 9451 of the tet site
+    */
+    const response = await login_response.then((value) => {
+        jwt = value.jwt;
+        console.log("the token is : " + jwt);
+        Promise.resolve(0);
+    });
+    return response;
+}
+// ======================================
+// Update Fixtures
+// ======================================
+//update_fixture_table()
+function update_fixture_table() {
+    const new_md_output = new Promise((resolve, reject) => {
+        resolve((0, fixtures_1.get_list_of_fixtures)());
+    });
+    new_md_output.then(value => {
+        let edit_community_form = {
+            auth: jwt,
+            community_id: 9451,
+            description: value
+        };
+        setTimeout(function () {
+            client.editCommunity(edit_community_form);
+            console.log(`Value of the table is\n+++++++++++++++\n${value}`);
+        }, 5000);
+    });
+}
+// ======================================
+// End Update Fixtures
+// ======================================
+// Daily Posts
+function create_daily_posts() {
+    let post_details = post_template;
+    const moment = require('moment'); // require
+    let utc_date_as_object = moment().utc().toObject();
+    let utc_day_of_week = moment().utc().day();
+    // to do If it is match day if ()
+    if (utc_day_of_week == 5) //check if its friday
+     {
+        post_details.name = "💬 Free Talk Friday";
+        post_details.body = "It's that time of the week!! What's the craic?";
+    }
+    else {
+        post_details.name = "🍻 The Lounge: Daily Discussion Thread";
+        post_details.body = "A thread to discuss daily events discussions etc. Please be civil and encourage discourse.";
+    }
+    post_details.auth = jwt;
+    let new_post = post_details;
+    client.createPost(new_post);
+}
+//
+// =========================================
+// Cron Jobs using manual JS client due to promise issue 
+//
+//
+// =========================================
+// Daily Cron 
+var cron = require('node-cron');
+cron.schedule('* 9 * * *', async () => {
+    update_fixture_table();
+    console.log('Run the Daily thread stuff; Friday special Free talk friday');
+    //let login_response = await js_client_login('sky_7_bot_testing')
+    //console.log('Login response was '+login_response)
+    let test = 'sky_7_bot_testing';
+    let login_response = new Promise((resolve, reject) => {
+        return resolve(js_client_login('sky_7_bot_testing'));
+    }).then((value) => {
+        create_daily_posts();
+        return (0);
+    });
+    // Check if its match day
+}, {
+    timezone: 'Europe/London',
+    scheduled: true
+});
+// =========================================
+// End of Cron 
+// =========================================
+app.get('/', (req, res) => {
+    res.send('the user name of the bot is:- ' + process.env.USERNAME);
+});
+const server = app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+});
+module.exports = server;
---
+++ src/fixtures.js
@@ -0,0 +1,498 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.get_list_of_fixtures = void 0;
+const ID_FOR_UNITED_API = 33;
+const API_KEY_FOR_RAPID = '5jmYs3r2MTmshlSTY0CJbRNCQ0typ1WOIWEjsnfoTt8iWvZgCz';
+//const API_KEY_FOR_RAPID = '5jmYs3r2MTmshlSTY0CJbRNCQ0typ1WOIWEjsnfoTt8iWvZgCq'; //fake key
+const RAPID_API_HOST = 'api-football-v1.p.rapidapi.com';
+const BASE_URL_FOR_API = 'https://' + RAPID_API_HOST + '/v3/';
+var output_to_export = '';
+function send_fixtures(list_of_fixtures) {
+    const md_markdown = require('json2md');
+    let columns = ['Op', 'Comp', ' ', 'Date', 'Score'];
+    let out_string = '|';
+    columns.forEach(element => {
+        out_string += element + '|';
+    });
+    out_string += '\n |';
+    columns.forEach(element => {
+        out_string += ':----|';
+    });
+    out_string += '\n';
+    list_of_fixtures.forEach(element => {
+        out_string += '|' + element.opponent + '|' + element.comp + '|' + element.type + '|' + element.date + '|' + element.score;
+        out_string += '| \n';
+    });
+    //console.log(out_string)
+    return (out_string);
+    console.log('sending fixtures');
+}
+//sample API output to work with Delete this in production
+let sample_api_output = {
+    "get": "fixtures",
+    "parameters": {
+        "next": "5",
+        "team": "33"
+    },
+    "errors": [],
+    "results": 5,
+    "paging": {
+        "current": 1,
+        "total": 1
+    },
+    "response": [
+        {
+            "fixture": {
+                "id": 1030298,
+                "referee": null,
+                "timezone": "UTC",
+                "date": "2023-07-12T15:00:00+00:00",
+                "timestamp": 1689174000,
+                "periods": {
+                    "first": null,
+                    "second": null
+                },
+                "venue": {
+                    "id": 11603,
+                    "name": "Ullevaal Stadion",
+                    "city": "Oslo"
+                },
+                "status": {
+                    "long": "Not Started",
+                    "short": "NS",
+                    "elapsed": null
+                }
+            },
+            "league": {
+                "id": 667,
+                "name": "Friendlies Clubs",
+                "country": "World",
+                "logo": "https://media-1.api-sports.io/football/leagues/667.png",
+                "flag": null,
+                "season": 2023,
+                "round": "Club Friendlies 1"
+            },
+            "teams": {
+                "home": {
+                    "id": 33,
+                    "name": "Manchester United",
+                    "logo": "https://media-3.api-sports.io/football/teams/33.png",
+                    "winner": null
+                },
+                "away": {
+                    "id": 63,
+                    "name": "Leeds",
+                    "logo": "https://media-3.api-sports.io/football/teams/63.png",
+                    "winner": null
+                }
+            },
+            "goals": {
+                "home": null,
+                "away": null
+            },
+            "score": {
+                "halftime": {
+                    "home": null,
+                    "away": null
+                },
+                "fulltime": {
+                    "home": null,
+                    "away": null
+                },
+                "extratime": {
+                    "home": null,
+                    "away": null
+                },
+                "penalty": {
+                    "home": null,
+                    "away": null
+                }
+            }
+        },
+        {
+            "fixture": {
+                "id": 1030300,
+                "referee": null,
+                "timezone": "UTC",
+                "date": "2023-07-19T13:00:00+00:00",
+                "timestamp": 1689771600,
+                "periods": {
+                    "first": null,
+                    "second": null
+                },
+                "venue": {
+                    "id": null,
+                    "name": "Murrayfield Stadium",
+                    "city": "Edinburgh"
+                },
+                "status": {
+                    "long": "Not Started",
+                    "short": "NS",
+                    "elapsed": null
+                }
+            },
+            "league": {
+                "id": 667,
+                "name": "Friendlies Clubs",
+                "country": "World",
+                "logo": "https://media-1.api-sports.io/football/leagues/667.png",
+                "flag": null,
+                "season": 2023,
+                "round": "Club Friendlies 1"
+            },
+            "teams": {
+                "home": {
+                    "id": 33,
+                    "name": "Manchester United",
+                    "logo": "https://media-1.api-sports.io/football/teams/33.png",
+                    "winner": null
+                },
+                "away": {
+                    "id": 80,
+                    "name": "Lyon",
+                    "logo": "https://media-2.api-sports.io/football/teams/80.png",
+                    "winner": null
+                }
+            },
+            "goals": {
+                "home": null,
+                "away": null
+            },
+            "score": {
+                "halftime": {
+                    "home": null,
+                    "away": null
+                },
+                "fulltime": {
+                    "home": null,
+                    "away": null
+                },
+                "extratime": {
+                    "home": null,
+                    "away": null
+                },
+                "penalty": {
+                    "home": null,
+                    "away": null
+                }
+            }
+        },
+        {
+            "fixture": {
+                "id": 1030302,
+                "referee": null,
+                "timezone": "UTC",
+                "date": "2023-07-22T21:00:00+00:00",
+                "timestamp": 1690059600,
+                "periods": {
+                    "first": null,
+                    "second": null
+                },
+                "venue": {
+                    "id": null,
+                    "name": "MetLife Stadium",
+                    "city": "East Rutherford, New Jersey"
+                },
+                "status": {
+                    "long": "Not Started",
+                    "short": "NS",
+                    "elapsed": null
+                }
+            },
+            "league": {
+                "id": 667,
+                "name": "Friendlies Clubs",
+                "country": "World",
+                "logo": "https://media-1.api-sports.io/football/leagues/667.png",
+                "flag": null,
+                "season": 2023,
+                "round": "Club Friendlies 1"
+            },
+            "teams": {
+                "home": {
+                    "id": 42,
+                    "name": "Arsenal",
+                    "logo": "https://media-3.api-sports.io/football/teams/42.png",
+                    "winner": null
+                },
+                "away": {
+                    "id": 33,
+                    "name": "Manchester United",
+                    "logo": "https://media-1.api-sports.io/football/teams/33.png",
+                    "winner": null
+                }
+            },
+            "goals": {
+                "home": null,
+                "away": null
+            },
+            "score": {
+                "halftime": {
+                    "home": null,
+                    "away": null
+                },
+                "fulltime": {
+                    "home": null,
+                    "away": null
+                },
+                "extratime": {
+                    "home": null,
+                    "away": null
+                },
+                "penalty": {
+                    "home": null,
+                    "away": null
+                }
+            }
+        },
+        {
+            "fixture": {
+                "id": 1030307,
+                "referee": null,
+                "timezone": "UTC",
+                "date": "2023-07-26T02:30:00+00:00",
+                "timestamp": 1690338600,
+                "periods": {
+                    "first": null,
+                    "second": null
+                },
+                "venue": {
+                    "id": null,
+                    "name": "Snapdragon Stadium",
+                    "city": "San Diego, California"
+                },
+                "status": {
+                    "long": "Not Started",
+                    "short": "NS",
+                    "elapsed": null
+                }
+            },
+            "league": {
+                "id": 667,
+                "name": "Friendlies Clubs",
+                "country": "World",
+                "logo": "https://media-1.api-sports.io/football/leagues/667.png",
+                "flag": null,
+                "season": 2023,
+                "round": "Club Friendlies 1"
+            },
+            "teams": {
+                "home": {
+                    "id": 33,
+                    "name": "Manchester United",
+                    "logo": "https://media-2.api-sports.io/football/teams/33.png",
+                    "winner": null
+                },
+                "away": {
+                    "id": 1837,
+                    "name": "Wrexham",
+                    "logo": "https://media-1.api-sports.io/football/teams/1837.png",
+                    "winner": null
+                }
+            },
+            "goals": {
+                "home": null,
+                "away": null
+            },
+            "score": {
+                "halftime": {
+                    "home": null,
+                    "away": null
+                },
+                "fulltime": {
+                    "home": null,
+                    "away": null
+                },
+                "extratime": {
+                    "home": null,
+                    "away": null
+                },
+                "penalty": {
+                    "home": null,
+                    "away": null
+                }
+            }
+        },
+        {
+            "fixture": {
+                "id": 1030312,
+                "referee": null,
+                "timezone": "UTC",
+                "date": "2023-07-27T02:30:00+00:00",
+                "timestamp": 1690425000,
+                "periods": {
+                    "first": null,
+                    "second": null
+                },
+                "venue": {
+                    "id": null,
+                    "name": "NRG Stadium",
+                    "city": "Houston, Texas"
+                },
+                "status": {
+                    "long": "Not Started",
+                    "short": "NS",
+                    "elapsed": null
+                }
+            },
+            "league": {
+                "id": 667,
+                "name": "Friendlies Clubs",
+                "country": "World",
+                "logo": "https://media-1.api-sports.io/football/leagues/667.png",
+                "flag": null,
+                "season": 2023,
+                "round": "Club Friendlies 1"
+            },
+            "teams": {
+                "home": {
+                    "id": 541,
+                    "name": "Real Madrid",
+                    "logo": "https://media-3.api-sports.io/football/teams/541.png",
+                    "winner": null
+                },
+                "away": {
+                    "id": 33,
+                    "name": "Manchester United",
+                    "logo": "https://media-2.api-sports.io/football/teams/33.png",
+                    "winner": null
+                }
+            },
+            "goals": {
+                "home": null,
+                "away": null
+            },
+            "score": {
+                "halftime": {
+                    "home": null,
+                    "away": null
+                },
+                "fulltime": {
+                    "home": null,
+                    "away": null
+                },
+                "extratime": {
+                    "home": null,
+                    "away": null
+                },
+                "penalty": {
+                    "home": null,
+                    "away": null
+                }
+            }
+        }
+    ]
+};
+function callback(err) {
+    console.log(err);
+}
+function get_type_of_fixture(home_team) {
+    if (home_team == 33) {
+        return "H";
+    }
+    else {
+        return "A";
+    }
+}
+function get_score(home_goal, away_goal) {
+    return home_goal.toString() + " - " + away_goal.toString();
+}
+async function get_fixtures() {
+    const axios = require('axios');
+    let endpoint = 'fixtures';
+    let api_url = BASE_URL_FOR_API + endpoint;
+    //querystring is needed 
+    //console.log("Iam running")
+    let query_string = { team: ID_FOR_UNITED_API
+        //	, season: 2023
+        //, last:2
+        ,
+        next: 5
+    };
+    const options = {
+        method: 'GET',
+        url: api_url,
+        params: query_string,
+        headers: {
+            'X-RapidAPI-Key': API_KEY_FOR_RAPID,
+            'X-RapidAPI-Host': RAPID_API_HOST,
+        },
+    };
+    axios
+        .request(options)
+        .then(function (data) {
+        //console.log(data)
+        let formatted_response = format_fixtures(data.data);
+        const markdown_response = send_fixtures(formatted_response);
+        //console.log(markdown_response)
+        //return markdown_response || ''
+        output_to_export = markdown_response;
+        /*
+        console.log(options);
+        console.log("");
+        console.log("===================================");
+        console.log("");
+        console.log(data);
+        */
+        //const json = JSON.stringify(data);
+        //const fs = require('fs');
+        //fs.writeFile('myjsonfile.json', json, 'utf8', callback);
+    })
+        .catch(function (error) {
+        console.error(error);
+    });
+}
+function format_fixtures(data) {
+    let fix_list = [];
+    //let working_copy =   sample_api_output
+    let working_copy = data || sample_api_output;
+    // console.log(data.data.response)
+    const moment = require('moment');
+    working_copy.response.forEach(fixt => {
+        let current_fixture = {
+            type: '',
+            comp: '',
+            date: '',
+            score: '',
+            opponent: ''
+        };
+        //fixt.teams.home.id 
+        current_fixture.type = get_type_of_fixture(fixt.teams.home.id);
+        if (fixt.goals.home == null || fixt.goals.away == null) {
+            current_fixture.score = ' - ';
+        }
+        else {
+            current_fixture.score = get_score(fixt.goals.home, fixt.goals.away);
+        }
+        current_fixture.date = (moment(fixt.fixture.date).format('MM/DD'));
+        if (current_fixture.type == 'H') {
+            //current_fixture.opponent = `![crest](${fixt.teams.away.logo}) ${fixt.teams.away.name}`
+            current_fixture.opponent = ` **${fixt.teams.away.name}**`;
+        }
+        else {
+            //current_fixture.opponent = `![crest](${fixt.teams.home.logo}) ${fixt.teams.home.name}`
+            current_fixture.opponent = `**${fixt.teams.home.name}**`;
+        }
+        if (fixt.league.id == 667) {
+            current_fixture.comp = "FR";
+            //todo add more leagues info
+        }
+        //console.log (current_fixture)
+        let date_for_sheets = moment(fixt.fixture.date, moment.ISO_8601).format('YYYY-DD-MM');
+        fix_list.push(current_fixture);
+    });
+    return fix_list;
+}
+//format_fixtures('');
+get_fixtures();
+async function get_list_of_fixtures() {
+    const promise_1 = new Promise((resolve, reject) => {
+        resolve(get_fixtures());
+    });
+    promise_1.then(value => {
+        console.log(`Exporting Value ${output_to_export}`);
+        return output_to_export;
+    });
+    return output_to_export;
+}
+exports.get_list_of_fixtures = get_list_of_fixtures;
